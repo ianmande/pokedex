c84271942edebca1cbb8d51f284a539333298be3
Ian Mande <isaac@mftech.io>
2023-12-07 00:26:24 -0400

    feat: added husky to setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'https://pokeapi.co',
+        port: '',
+        pathname: '/api/v2/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
