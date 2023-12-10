2af0f1452168676763b3aa53a0721afb044ae797
Ian Mande <isaac@mftech.io>
2023-12-09 21:40:26 -0400

    feat: change cinfig

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const TOKEN_API = process.env.API_TOKEN || '';
 
-export const APP_HOST = `${process.env.NEXT_PUBLIC_APP_HOST}:${process.env.NEXT_PUBLIC_PORT}`;
+export const APP_HOST = process.env.NEXT_PUBLIC_APP_HOST;
 
 export const USERS_DB_PATH = 'src/db/user.json';
